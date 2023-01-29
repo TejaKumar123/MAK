e06f2cb9c27c24b8afd3dc9f4cafcb9bbcb6e34d
TejaKumar123 <97971538+TejaKumar123@users.noreply.github.com>
2023-01-29 23:50:35 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,66 @@
+
+function sidepane(n){
+s=document.getElementById('sidepane');
+if(n==1){
+s.style.width='280px';
+s.style.border="1px solid black";
+}
+else{
+s.style.width="0px";
+s.style.border="none";
+}
+
+}
+sign_index=0;
+function open_sign_up(k){
+m=document.getElementById("signup");
+n=document.getElementById("sign1");
+if(k==1){
+m.style.display="flex";
+n.style.animation="popup 0.30s linear 0s 1 normal";
+opensign(sign_index);
+}
+else{
+m.style.display="none";
+n.style.animation="";
+}
+
+}
+
+
+function opensign(l){
+m=document.getElementsByClassName("loginup");
+sign_index=l;
+if(l==0){
+document.getElementById("signin").style.display="block";
+document.getElementById("signin").style.animation="opac 0.25s linear 0s 1 normal";
+document.getElementById("signin1").style.display="none";
+document.getElementById("signin1").style.animation="";
+m[0].style.backgroundColor="#39bed0";
+m[0].style.boxShadow="0px 1px 3px black";
+m[1].style.boxShadow="none";
+m[1].style.backgroundColor="#2999bf";
+
+
+}
+
+else{
+document.getElementById("signin").style.display="none";
+document.getElementById("signin").style.animation="";
+document.getElementById("signin1").style.display="block";
+document.getElementById("signin1").style.animation="opac 0.25s linear 0s 1 normal";
+m[1].style.backgroundColor="#39bed0";
+m[1].style.boxShadow="0px 1px 3px black";
+m[0].style.boxShadow="none";
+m[0].style.backgroundColor="#2999bf";
+}
+
+}
+
+
+
+
+
+
+
+
